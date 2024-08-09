40e0700cd6cece87835ad4c4c6278918ca06b0e8
Haseeb Siddiqi <haseebs727@gmail.com>
2024-08-08 23:23:06 -0400

    First commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import {  Routes, Route } from "react-router-dom";
+import Home from './pages/Home'
+import Contact from './pages/Contact'
+import About from './pages/About'
+import NoPage from './pages/NoPage'
+import Navbar from "./components/Navbar";
+
+
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <div>
+    
+        <Routes>
+          <Route index element ={<Home/>} />
+          <Route path ="/home" element={<Home/> } />
+          <Route path ="/about" element={<About/> } />
+          <Route path ="/contact" element={<Contact/> } />
+          <Route path ="*" element={<NoPage/>} />
+        </Routes>
+      
+    </div>
+    </>
+  );
+}
+
+export default App;
