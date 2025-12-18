1cf725ac5b23a8bff24da36678508f8523d60b3a
Haseeb Siddiqi <haseebs727@gmail.com>
2025-12-18 18:47:42 -0500

    Editied footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@
 export default function Footer(){
     return(
       <footer className="footer">
-        <p>&copy; 2024 Haseeb Siddiqi. All rights reserved. | This website was created by Haseeb Siddiqi.</p>
+        <p>&copy; 2024 Haseeb Siddiqi. All rights reserved.</p>
 
       </footer>
     )
